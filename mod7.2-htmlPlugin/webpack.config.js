09641ed607dc92cc17d96c2c5cb8b2f14a1e7fd5
shutsmanroman <roman13275@gmail.com>
2024-03-11 17:53:29 +0200

    mod7.2: html plugin

---
+++ mod7.2-htmlPlugin/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        product: './src/products.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/index.html'),
+            chunks: ['index'],
+            inject: true,
+            filename: 'index.html'
+        }),
+        new htmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src/products.html'),
+            chunks: ['product'],
+            inject: true,
+            filename: 'products.html'
+        })
+    ],
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /.(png|jpeg|gif|svg)$/,
+    //             type: 'asset/resource',
+    //             generator: {
+    //                 filename: 'assets/[hash][ext]'
+    //             }
+    //         }
+    //     ]
+    // }
+}
