7f35ccb7f5054e301e417741aa10331b7d819829
shutsmanroman <roman13275@gmail.com>
2024-03-11 15:33:40 +0200

    mod4: scss modules

---
+++ mod4-scssLoaders/src/index.js
@@ -0,0 +1,21 @@
+import style from './index.scss';
+import './clearButton';
+import _ from 'lodash';
+
+const btn1 = document.getElementById('buttonClicked2')
+
+btn1.addEventListener('click', function(){
+    const el = document.getElementById('header');
+    el.innerHTML = 'Updated code';
+
+    const list = document.getElementById('list');
+    const listItems = ['apple', 'banana'];
+    list.innerHTML = 'Updated code';
+    _.forEach(listItems, e => {
+        const el = document.createElement('li');
+        el.innerHTML = e;
+        list.appendChild(el);
+    })
+})
+
+btn1.classList.add([style.button])
