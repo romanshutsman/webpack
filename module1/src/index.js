b6948541846b6f034beb8b7c9279f841468916a6
shutsmanroman <roman13275@gmail.com>
2024-03-11 14:14:12 +0200

    init first module

---
+++ module1/src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+
+document.getElementById('buttonClicked2').addEventListener('click', function(){
+    const el = document.getElementById('header');
+    el.innerHTML = 'Updated code';
+
+    const list = document.getElementById('list');
+    const listItems = ['apple', 'banana'];
+    list.innerHTML = 'Updated code';
+    _.forEach(listItems, e => {
+        const el = document.createElement('li');
+        el.innerHTML = e;
+        list.appendChild(el);
+    })
+})
