e36a7b3e7f1ad7ae0cbe7107302f1eb0ce4dcac6
shutsmanroman <roman13275@gmail.com>
2024-03-11 15:18:36 +0200

    mod3: css modules: global

--- mod3-cssLoaders/src/clearButton.js
+++ mod3-cssLoaders/src/clearButton.js
@@ -1,7 +1,7 @@
-import style from './clearButton.css'
+// import style from './clearButton.css'
 const el = document.createElement('button');
 el.innerHTML = 'Clear';
-el.classList.add([style.button])
+el.classList.add(['button'])
 el.onclick = function () {
     alert('clicked')
 }
